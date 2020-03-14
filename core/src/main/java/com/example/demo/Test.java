986ace09fc52ac95d06f66a5268271684c5a32e1
chenming1237 <464598056@qq.com>
2020-03-14 15:41:20 +0800

    test111

--- core/src/main/java/com/example/demo/Test.java
+++ core/src/main/java/com/example/demo/Test.java
@@ -3,7 +3,7 @@ package com.example.demo;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("SSASASASA");
+        System.out.println("SSASASASAxxx");
     }
 
     public void t(){

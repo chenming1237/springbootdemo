dbd1eb3e99f89b1a70813faccd478ce286e4da14
chenming1237 <464598056@qq.com>
2020-03-14 21:47:53 +0800

    2222222

--- start/src/main/java/com/example/demo/SpringbootdemoApplication.java
+++ start/src/main/java/com/example/demo/SpringbootdemoApplication.java
@@ -1,23 +1,19 @@
 package com.example.demo;
 
-import com.example.demo.controller.TestController;
-import com.example.demo.test.Test1Controller;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@ComponentScan(value = "com.example.demo.dao")
+@RestController
+@MapperScan(value = "com.example.demo.dao")
 public class SpringbootdemoApplication {
 
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(SpringbootdemoApplication.class,args);
+		SpringApplication.run(SpringbootdemoApplication.class,args);
 	}
 
 	@RequestMapping("/hello")
